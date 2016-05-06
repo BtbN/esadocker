34adc6fc4c121fa612e5dac5f635197d0a79711d
BtbN <btbn@btbn.de>
2016-05-06 21:13:06 +0200

    Changes

--- nginx/ffmpeg_wrapper.sh
+++ nginx/ffmpeg_wrapper.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+[ -n "$2" ] && RTMP_TARGET="$2"
+
 exec /usr/bin/ffmpeg -i "$1" \
 	-threads 0 \
 	-c:v libx264 -preset:v fast -pix_fmt:v yuv420p -s:v 1280x720 -r:v 60 -g:v 120 -b:v 3500k -minrate:v 3500k -maxrate:v 3500k -bufsize:v 3500k \
 	-c:a aac -b:a 160k -ar:a 44100 \
 	-f flv "${RTMP_TARGET}"
+
