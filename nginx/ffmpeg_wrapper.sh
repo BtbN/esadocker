279f492990ef530889380b30b2e21717357cc557
BtbN <btbn@btbn.de>
2016-05-07 18:43:49 +0200

    Log ffmpeg output

--- nginx/ffmpeg_wrapper.sh
+++ nginx/ffmpeg_wrapper.sh
@@ -6,5 +6,5 @@ exec /usr/bin/ffmpeg -i "$1" \
 	-threads 0 \
 	-c:v libx264 -preset:v fast -pix_fmt:v yuv420p -s:v 1280x720 -r:v 60 -g:v 120 -b:v 3500k -minrate:v 3500k -maxrate:v 3500k -bufsize:v 3500k \
 	-c:a aac -b:a 160k -ar:a 44100 \
-	-f flv "${RTMP_TARGET}"
+	-f flv "${RTMP_TARGET}" >/config/ffmpeg.log 2>&1
 
