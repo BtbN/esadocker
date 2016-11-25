68a6e50b50af11c615ec21ecbd6c1fd0192f2bc6
BtbN <btbn@btbn.de>
2016-11-25 15:00:51 +0100

    Update nginx

--- nginx/ffmpeg_wrapper.sh
+++ nginx/ffmpeg_wrapper.sh
@@ -9,7 +9,7 @@ echo >> /config/ffmpeg.log
 
 exec /usr/bin/ffmpeg -i "$1" \
 	-threads 0 \
-	-c:v libx264 -preset medium -pix_fmt yuv420p -profile main -s 1280x720 -r 60 -g 120 -b:v 3500k -minrate 3500k -maxrate 3500k -bufsize 3500k \
+	-c:v libx264 -preset medium -pix_fmt yuv420p -x264opts "nal-hrd=cbr:force-cfr=1" -profile main -s 1280x720 -r 60 -g 120 -b:v 3500k -maxrate 3500k -bufsize 3500k \
 	-c:a aac -b:a 160k -ar 44100 \
 	-f flv "${RTMP_TARGET}" >>/config/ffmpeg.log 2>&1
 
